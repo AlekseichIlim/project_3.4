dfa6f41b47ea202a7833597e6aa07e2f1d2f20f9
Alekseich <431410@mail.ru>
2024-03-19 22:42:30 +0800

    finish func

--- str_func.py
+++ str_func.py
@@ -1,7 +1,11 @@
 def upstr(value):
+
     '''в верхний регистр все буквы'''
+
     return value.upper()
 
 def titlestr(value):
+
     '''в верхний регистр первых букв'''
+
     return value.title()
